37d22a55210b7e4d8d59c066d756d6553297fc25
叶晓峰 <13751732491@163.com>
2017-06-28 09:53:56 +0800

    添加字符串操作类

---
+++ ChaYeFeng/CYFStringHelper/CYFStringHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChaYeFeng
+{
+    public static class CYFStringHelper
+    {
+        /// <summary>
+        /// 将字符串集合用指定字符串分隔，默认为逗号
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="separator"></param>
+        /// <returns></returns>
+        public static string SeparateString(this ICollection<string> source,string separator = ",")
+        {
+            string result = string.Empty;
+            if (string.IsNullOrEmpty(separator))
+                separator = ",";
+            result = string.Join(separator, source.ToArray());
+            return result;
+        }
+    }
+}
